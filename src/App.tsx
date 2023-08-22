27f9f7697ddae3b982ce8a4cdcd1ee7c8f0eba68
maia-tarapipipi <tarapipipim@gmail.com>
2023-08-22 14:50:13 +1200

    boilerplate set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from '@auth0/auth0-react'
+
 
 function App() {
+  const { isAuthenticated } = useAuth0()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      {isAuthenticated ? (
+// header
+// outlet (dashboard and chat app)
+// 
+      ) : (
+// header
+// login
+// register
+      )
+      }
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
