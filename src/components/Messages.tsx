4fb710f909af4a760833d34937a62be6535a63f8
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-28 13:41:21 +1200

    trying to connect to socket server

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import io from 'socket.io-client'
+import React from "react";
+import io from "socket.io-client";
 
-//may need to pass the URL
-// const url = 'http://loaclhost:3000'
-// const url = 'deployed site  url'
-
-const socket = io()
+const socket = io();
 
 export default function Messages() {
-
-  
+  const [messages, setMessages] = React.useState([]);
+  socket.on("message", (msg) => {
+    setMessages((messages) => [...messages, msg]);
+  });
 
   return (
     <div>
       heres where the messages will go!
-      {/* messages.map((msg) => 
-    <p>msg.name</p>
-    <p>msg.message</p>
-    <p>msg.date</p>
-    ) */}
+      {messages.map((msg, id) => (
+        <div key={id}>
+          <p>msg.name</p>
+          <p>msg.message</p>
+          <p>msg.date</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
