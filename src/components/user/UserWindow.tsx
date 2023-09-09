74dfbc07d27828bba49eb7a9c9ce5e776750b0f7
maia-tarapipipi <tarapipipim@gmail.com>
2023-09-09 13:11:04 +1200

    styling changes

--- src/components/user/UserWindow.tsx
+++ src/components/user/UserWindow.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 export default function UserWindow() {
   // need to add hover styling
   // this will have the list of friends which the user will have connected with
-  return <div className="bg-[#95B93C] w-1/3">xxx</div>
+  return <div className="bg-[#B7DB5B] w-1/3"></div>
 }
